390cfecb99108aec0ac401eff03b889cfdcef6d3
studentp_10 <IamSid25>
2018-08-31 11:06:10 +0530

    Initial Commit

---
+++ IosDay16/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  IosDay16
+//
+//  Created by Student P_10 on 30/08/18.
+//  Copyright © 2018 priya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
